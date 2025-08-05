2844dc19ab379cb3c7b861669d73803d781bd767
Roland Kwitt <Roland.Kwitt@sbg.ac.at>
2025-08-05 09:46:33 +0000

    FIX: generate_figure_eight_2d_points naming

--- tests/test_landmarks.py
+++ tests/test_landmarks.py
@@ -4,21 +4,21 @@ import numpy as np
 
 from flooder import (
     generate_landmarks,
-    generate_figure_eight_2D_points,
+    generate_figure_eight_2d_points,
 )
 
 
-@pytest.mark.parametrize("n_landmarks", [64, 256, 1024])
-def test_generate_landmarks(n_landmarks):
+@pytest.mark.parametrize("n_lms", [64, 256, 1024])
+def test_generate_landmarks(n_lms):
     """
     Test landmark generation via FPS.
     """
     torch.manual_seed(42)
     np.random.seed(42)
 
-    X = generate_figure_eight_2D_points(10000)
-    L = generate_landmarks(X, n_landmarks)
+    X = generate_figure_eight_2d_points(10000)
+    L = generate_landmarks(X, n_lms)
 
-    assert L.shape == (n_landmarks, 2), f"Wrong shape {L.shape}"
+    assert L.shape == (n_lms, 2), f"Wrong shape {L.shape}"
     assert L.dtype == torch.float32, f"Wrong datatype {L.dtype}"
     assert L.device == X.device, f"Wrong device {L.device}"
