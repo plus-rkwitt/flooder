1320e8db51fb3c9fcdf4f007abd16c6cf99e4902
Roland Kwitt <Roland.Kwitt@sbg.ac.at>
2025-08-05 08:48:51 +0000

    ENH: variable naming in example_01

--- examples/example_01_cheese_3d.py
+++ examples/example_01_cheese_3d.py
@@ -22,9 +22,14 @@ RESET = "\033[0m"
 
 
 def main():  # pylint: disable=missing-function-docstring
-    n_ps = [10000, 100000, 1000000, 10000000]  # Number of flood sources / data points
-    n_l = 1000  # Number of landmarks to use
-    b_sizes = [1024, 1024, 32, 2]  # Batch sizes for flood complex computation
+    n_pts_list = [
+        10000,
+        100000,
+        1000000,
+        10000000,
+    ]  # Number of flood sources / data points
+    n_lms = 1000  # Number of landmarks to use
+    batch_sizes = [1024, 1024, 32, 2]  # Batch sizes for flood complex computation
 
     rect_min = (0.0, 0.0, 0.0)
     rect_max = (1.0, 1.0, 1.0)
@@ -38,10 +43,10 @@ def main():  # pylint: disable=missing-function-docstring
 
     print(f"{YELLOW}Alpha PH vs. Flood PH timing on cheese")
     print(f"{YELLOW}--------------------------------------")
-    for i, n_p in enumerate(n_ps):
+    for i, n_pts in enumerate(n_pts_list):
         for rep in range(5):
             points, _, _ = generate_swiss_cheese_points(
-                n_p, rect_min, rect_max, k, void_radius_range, device=DEVICE
+                n_pts, rect_min, rect_max, k, void_radius_range, device=DEVICE
             )
 
             startt = timer()
@@ -53,14 +58,14 @@ def main():  # pylint: disable=missing-function-docstring
             alpha.compute_persistence()
             t2 = timer() - startt
             print(
-                f"{RED}{n_p:8d} points (try {rep}) | "
+                f"{RED}{n_pts:8d} points (try {rep}) | "
                 f"Complex (Alpha): {t1:6.2f} sec | "
                 f"PH (Alpha): {t2:6.2f} sec{RESET}"
             )
             results.append(
                 {
                     "rep": rep,
-                    "n_p": n_p,
+                    "n_pts": n_pts,
                     "method": "Alpha",
                     "complex_time": t1,
                     "ph_time": t2,
@@ -71,11 +76,13 @@ def main():  # pylint: disable=missing-function-docstring
 
             points = points.to(DEVICE)
             # GPU warmup
-            out_complex = flood_complex(points[:10000], n_l, batch_size=b_sizes[i])
+            out_complex = flood_complex(
+                points[:10000], n_lms, batch_size=batch_sizes[i]
+            )
             torch.cuda.synchronize()
 
             startt = timer()
-            out_complex = flood_complex(points, n_l, batch_size=b_sizes[i])
+            out_complex = flood_complex(points, n_lms, batch_size=batch_sizes[i])
 
             st = SimplexTree()
             for simplex in out_complex:
@@ -87,14 +94,14 @@ def main():  # pylint: disable=missing-function-docstring
             st.compute_persistence()
             t2 = timer() - startt
             print(
-                f"{BLUE}{n_p:8d} points (try {rep}) | "
+                f"{BLUE}{n_pts:8d} points (try {rep}) | "
                 f"Complex (Flood): {t1:6.2f} sec | "
                 f"PH (Flood): {t2:6.2f} sec{RESET}"
             )
             results.append(
                 {
                     "rep": rep,
-                    "n_p": n_p,
+                    "n_pts": n_pts,
                     "method": "Flood",
                     "complex_time": t1,
                     "ph_time": t2,
@@ -104,7 +111,7 @@ def main():  # pylint: disable=missing-function-docstring
 
     df = pd.DataFrame(results)
     summary = (
-        df.groupby(["n_p", "method"])
+        df.groupby(["n_pts", "method"])
         .agg(
             complex_mean=("complex_time", "mean"),
             complex_std=("complex_time", "std"),
@@ -122,7 +129,7 @@ def main():  # pylint: disable=missing-function-docstring
     )
     print(f"\n{YELLOW}Summary of Timings (mean ± std over 5 repetitions){RESET}")
     print(
-        summary[["n_p", "method", "Complex Time (s)", "PH Time (s)"]].to_string(
+        summary[["n_pts", "method", "Complex Time (s)", "PH Time (s)"]].to_string(
             index=False
         )
     )
