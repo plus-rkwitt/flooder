e0c156c796acab273de27b3c33fdd5b80c5eca56
Roland Kwitt <Roland.Kwitt@sbg.ac.at>
2026-02-12 12:54:16 +0000

    Deployed 04336ad with MkDocs version: 1.6.1

--- animation/generate_csvs.py
+++ animation/generate_csvs.py
@@ -1,13 +1,11 @@
-from flooder import generate_figure_eight_points_2d, flood_complex, generate_landmarks, generate_swiss_cheese_points
-import matplotlib.pyplot as plt
+from flooder import generate_figure_eight_points_2d, flood_complex, generate_landmarks
 import numpy as np
 
-pts = generate_figure_eight_points_2d(200, centers=((0.3,.5), (0.6,0.5)))
-# pts, _ ,_ = generate_swiss_cheese_points(250, (0,0),(1,1))
+pts = generate_figure_eight_points_2d(200, centers=((0.3, .5), (0.6, 0.5)))
 lms = generate_landmarks(pts, 25)
 f_dict = flood_complex(pts, lms)
-edges = [(*i, j) for i, j in f_dict.items() if len(i)==2]
-triangles = [(*i, j) for i, j in f_dict.items() if len(i)==3]
+edges = [(*i, j) for i, j in f_dict.items() if len(i) == 2]
+triangles = [(*i, j) for i, j in f_dict.items() if len(i) == 3]
 
 np.savetxt('points.csv', pts, delimiter=',', fmt='%.8f')
 np.savetxt('landmarks.csv', lms, delimiter=',', fmt='%.8f')
